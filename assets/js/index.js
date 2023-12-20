8dbdd47fe2ad0f26ca8e0e7f64e5e9affe2b69d3
ktolico <130526956+ktolico@users.noreply.github.com>
2023-12-20 12:49:19 -0500

    avance1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
 console.log("Entro index.js");
+
+fetch('https://dev4humans.com.mx/api/clases/productos', {method: "GET"})
+    .then(response => response.json())
+    .then(data => {
+        console.log("La promesa se resolvio");
+        const rawData = data;
+        console.log(rawData);
+        console.log(rawData.data.products);
+    }).catch(error => console.log(error));
